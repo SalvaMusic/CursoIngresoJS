1eb9465afe01c759cdd5ba5e8f1029998769c006
SalvaMusic <36174261+SalvaMusic@users.noreply.github.com>
2018-02-06 22:04:32 -0300

    Ejercicios 6, 7, 8, 9 y 10

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,19 @@ transformarlos a enteros (parseInt) y Sumarlos.
 Mostar el resulto por medio de "ALERT"*/
 function sumar()
 {
-
+	//definir variables
+	var resultado;
+	var numeroUno;
+	var numeroDos;
+	//tomar datos
+	numeroUno=document.getElementById('numeroUno').value;
+	numeroDos=document.getElementById('numeroDos').value;
+	//transformar, parsear
+	numeroUno=parseInt(numeroUno);
+	numeroDos=parseInt(numeroDos);
+	//operar
+	resultado=numeroUno+numeroDos;
+	//mostrar
+	alert(resultado);
 }
 
