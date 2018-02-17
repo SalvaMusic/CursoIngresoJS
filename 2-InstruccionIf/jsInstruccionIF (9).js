edfad369c9f57689145def72c143616ec8bc764b
SalvaMusic <36174261+SalvaMusic@users.noreply.github.com>
2018-02-17 07:25:15 -0300

    Ejercicio 9
    No me sale

--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,6 +1,12 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	//var numero//
+
+	//numero=return Math.random(0-10) * (0-10) + 0;//
+
+	//document.getElementById('fromIngreso').value=numero;//
+	//document.getElementById("demo").innerHTML = Math.random();//
+	document.getElementById("fromIngreso").value = Math.floor(Math.random(1) * 10);
 
 }//FIN DE LA FUNCIÓN
