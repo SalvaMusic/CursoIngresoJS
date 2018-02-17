74f5aee2c638adb6c3a875a5643077b31449854c
SalvaMusic <36174261+SalvaMusic@users.noreply.github.com>
2018-02-17 12:08:58 -0300

    Ejercicios del 1 al 6
    Aprendimos a usar el Switch

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -1,11 +1,15 @@
 function Mostrar()
 {
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+	//tomo la edad  
+	var mesDelAño = document.getElementById('mes').value;
 
-//alert (mesDelAño);
-	
-	
-
-
+	switch(mesDelAño)
+	{
+		case "Febrero" :
+			alert("Este mes no tiene mas de 29 días");
+			break;
+		default :
+			alert("Este mes tiene 30 o más días");
+	}
+	//alert (mesDelAño);
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -3,7 +3,26 @@ function Mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
-//alert (mesDelAño);
+switch(mesDelAño)
+{
+	case "Febrero" :
+		alert("Tiene 28 dìas");
+		break;
+	case "Enero" :
+	case "Marzo" :
+	case "Mayo" :
+	case "Julio" :
+	case "Agosto" :
+	case "Octubre" :
+	case "Diciembre" :
+		alert("Tiene 31 días");
+		break;
+	default:
+		alert("Tiene 30 días");
+}
+
+
+
 	
 	
 
