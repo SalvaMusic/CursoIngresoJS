c8a95db7bdb6ffc7da1c2b33d3acd01be6e16d81
SalvaMusic <36174261+SalvaMusic@users.noreply.github.com>
2018-02-21 10:19:50 -0300

    ejercicio 10
    Entendi el uso del while
    use document.write
    100% hecho desde cero

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -2,15 +2,61 @@ function Mostrar()
 {
 
 	var contador=0;
+	var numero;
+	var positivos=0;
+	var negativos=0;
+	var cantidadPositivos=0;
+	var cantidadNegativos=0;
+	var cantidadCeros=0;
+	var cantidadPares=0;
+	var promedioPositivos;
+	var promedioNegativos;
+	var diferenciaPositivosNegativos;
 	//declarar contadores y variables 
 	
 	var respuesta="si";
 
 	while(respuesta!="no")
 	{
-		
-	
+		numero=prompt("Ingrese un numero");
+		numero=parseInt(numero);
+		if(numero<0)
+		{
+			cantidadNegativos++;
+			negativos=negativos+numero;
+		}
+		else
+		{
+			if(numero>0)
+			{
+				cantidadPositivos++;
+				positivos=positivos+numero;
+			}
+			else
+			{
+				cantidadCeros++;
+			}
+		}
+		if(numero%2==0)
+		{
+			cantidadPares++;
+		}
+
+		respuesta=prompt("No ingrese 'no' para continuar")
 	}
+	promedioPositivos=positivos/cantidadPositivos;
+	promedioNegativos=negativos/cantidadNegativos;
+	diferenciaPositivosNegativos=positivos-negativos;
+
+	document.write("<br>Suma de los positivos: "+positivos);
+	document.write("<br>Suma de los negativos: "+negativos);
+	document.write("<br>Cantidad de positivos: "+cantidadPositivos);
+	document.write("<br>Cantidad de negativos: "+cantidadNegativos);
+	document.write("<br>Cantidad de Ceros: "+cantidadCeros);
+	document.write("<br>Cantidad de Numeros pares: "+cantidadPares);
+	document.write("<br>Promedio de positivos: "+promedioPositivos);
+	document.write("<br>Promedio de negativos: "+promedioNegativos);
+	document.write("<br>Diferencia entre positivos y negativos: "+diferenciaPositivosNegativos);
 
 
 
