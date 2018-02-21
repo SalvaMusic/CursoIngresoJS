a0bd0b3a04a80f87f14cf0202922b22cfed6629b
SalvaMusic <36174261+SalvaMusic@users.noreply.github.com>
2018-02-20 22:00:17 -0300

    Ejercicios 8 y 9

--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -4,11 +4,26 @@ function Mostrar()
 	var contador=0;
 	var positivo=0;
 	var negativo=1;
-	
+	var numero;
 	var respuesta='si';
 
+	while(respuesta == 'si')
+	{
+		numero=prompt("Ingrese un número");
+		if(numero < 0)
+		{
+			negativo=negativo*numero;
+		}
+		else
+		{
+			numero=parseInt(numero);
+			positivo=positivo+numero;
+		}
 
-document.getElementById('suma').value=positivo;
-document.getElementById('producto').value=negativo;
+		respuesta=prompt("Ingrese (si) para seguir operando");
+	}
+
+	document.getElementById('suma').value=positivo;
+	document.getElementById('producto').value=negativo;
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -2,17 +2,39 @@ function Mostrar()
 {
 
 	var contador=0;
+	var numero;
+	var maximo;
+	var minimo;
 	// declarar variables
 	
 	var respuesta='si';
 
 	while(respuesta!='no')
 	{
-		
-	
+		contador++;
+		numero=prompt("Ingrese un número");
+
+		if(contador == 1)
+		{
+			maximo=numero;
+			minimo=numero;
+		}
+		else
+		{
+			if(numero > maximo)
+			{
+				maximo=numero;
+			}
+			if(numero < minimo)
+			{
+				minimo=numero;
+			}
+		}//if(contador == 1)
+
+		respuesta=prompt("No ingrese (no) para seguir operando");
 	}
 
-
-
+	document.getElementById('maximo').value=maximo;
+	document.getElementById('minimo').value=minimo;
 
 }//FIN DE LA FUNCIÓN
