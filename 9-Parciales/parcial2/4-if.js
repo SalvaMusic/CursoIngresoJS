2fb4a622a415d95a808b901daffb447e7046fd2b
SalvaMusic <36174261+SalvaMusic@users.noreply.github.com>
2018-02-27 21:45:34 -0300

    4

--- 9-Parciales/parcial2/4-if.js
+++ 9-Parciales/parcial2/4-if.js
@@ -1,7 +1,49 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 4-if");
+	/* ingresar dos numeros. 
+	si son iguales se concatenan.
+	si el primero es ayor que el segundo se multiplican.
+	si el segundo es mas grande se restan.
+	si la multiplicacion da un numero par, ademas de mostrar el resultado, se muestra el mensaje "es par".
+	alert("Funciona 4-if");*/
+
+	var numeroUno=prompt("Ingrese el primer número");
+	var numeroDos=prompt("Ingrese el segundo número");
+	var resultadoConcatenado;
+	var resultadoMultiplicado;
+	var resultadoResta;
+	var multiplicacionPar;
+
 	
+
+	if(numeroUno == numeroDos)
+	{
+		resultadoConcatenado=numeroUno+numeroDos;
+		alert("La concatenacion es: "+resultadoConcatenado);
+	}
+	else
+	{
+		numeroUno=parseInt(numeroUno);
+		numeroDos=parseInt(numeroDos);
+		if(numeroUno>numeroDos)
+		{
+			resultadoMultiplicado=numeroUno*numeroDos;
+			if(resultadoMultiplicado%2==0)
+			{
+				alert("La multiplicacion es: "+resultadoMultiplicado+" es par");
+			}
+			else
+			{
+				alert("La multiplicacion es: "+resultadoMultiplicado);
+			}
+
+		}
+		else
+		{
+			resultadoResta=numeroUno-numeroDos;
+			alert("La resta es: "+resultadoResta);
+		}
+	}//if(numeroUno == numeroDos)
 }
 
