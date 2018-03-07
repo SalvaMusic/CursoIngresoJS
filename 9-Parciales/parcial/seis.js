b54af9406c234b5e95bace482f64cc884d4315e7
SalvaMusic <36174261+SalvaMusic@users.noreply.github.com>
2018-03-07 15:52:01 -0300

    6 y 7

--- 9-Parciales/parcial/seis.js
+++ 9-Parciales/parcial/seis.js
@@ -1,4 +1,30 @@
 function Mostrar()
 {
+	var importe;
+	var contador=0;
+	var mayor=0;
+	var menor=0;
 
+	while(contador < 24)
+	{
+		contador ++;
+		importe=prompt("ingrese importe del día "+contador);
+		if (contador==1)
+		{
+			mayor=importe;
+			menor=importe;
+		} 
+		else 
+		{
+			if(importe>mayor)
+			{
+				mayor=importe;
+			}
+			if (importe<menor)
+			{
+				menor=importe;
+			}
+		}
+	}
+	document.getElementById('importeFinal').value=("Mayor: "+mayor+", Menor: "+menor);
 }
